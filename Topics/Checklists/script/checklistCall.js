86a3b083420c1f21b31b6479478f90a482b26a74
kbjarkefur <kbrkbr000@gmail.com>
2017-11-23 17:37:51 +0100

    function on array result

--- Topics/Checklists/script/checklistCall.js
+++ Topics/Checklists/script/checklistCall.js
@@ -15,6 +15,7 @@ $(function(){
 
       if(json.message == "Not Found" ) {
         $('#ghapidata').html("<h2>Checklist not found</h2>");
+
       } else {
 
         console.log(json);
@@ -23,6 +24,33 @@ $(function(){
         var checklist = JSON.parse("[" + checklistSTR + "]");
 
         console.log(checklist);
+        console.log(checklist.length);
+
+        outputPageContent();
+
+        //Add functions that test format of the checklist
+
+
+
+        function outputPageContent() {
+
+          var outhtml = ''
+
+          console.log("output!");
+          if(checklist.length == 0) {
+            outhtml = outhtml + '<p>Checklist of lenght 0 or incorrectly read</p>';
+
+          } else {
+            console.log("Tabletime!");
+            outhtml = outhtml + '<table>'
+            for (var i = 0; i < checklist.length; i++) {
+              console.log(checklist[i]);
+                outhtml = outhtml + '<tr><td>' + checklist[i][0] + '</td><td>' + checklist[i][1] + '</td></tr>'
+            };
+            outhtml = outhtml + '</table>'
+          }
+          $('#ghapidata').html(outhtml);
+        }
       }
     }); // end requestJSON Ajax call
   }); // end click event handler
