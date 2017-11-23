aa5a36d496a28743777fbcdd23bf8d71a575b338
kbjarkefur <kbrkbr000@gmail.com>
2017-11-23 17:10:06 +0100

    move javascript to seperate file

---
+++ Topics/Checklists/script/checklistCall.js
@@ -0,0 +1,39 @@
+$(function(){
+  $('#ghsubmitbtn').on('click', function(e){
+
+    console.log("click!");
+    e.preventDefault();
+
+    var checklistname = $('#chcklistName').val();
+
+
+    var requri   =   'https://api.github.com/repos/worldbank/DIMEwiki/contents/Topics/Checklists/checklists/'+ checklistname + '.json?ref=checklistAjax'
+
+
+    requestJSON(requri, function(json) {
+
+
+      if(json.message == "Not Found" ) {
+        $('#ghapidata').html("<h2>Checklist not found</h2>");
+      } else {
+
+        console.log(json);
+
+        var checklistSTR = atob(json.content);
+        var checklist = JSON.parse("[" + checklistSTR + "]");
+
+        console.log(checklist);
+      }
+    }); // end requestJSON Ajax call
+  }); // end click event handler
+
+  function requestJSON(url, callback) {
+    $.ajax({
+
+      url: url,
+      complete: function(xhr) {
+        callback.call(null, xhr.responseJSON);
+      }
+    });
+  }
+});
