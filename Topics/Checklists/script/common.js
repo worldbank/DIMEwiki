1f09147c53458af781706278e3aa896bbe85dae9
kbjarkefur <kbrkbr000@gmail.com>
2017-12-02 13:59:19 +0100

    common.js table footer and comments

--- Topics/Checklists/script/common.js
+++ Topics/Checklists/script/common.js
@@ -1,7 +1,8 @@
 
 /* Functions creating divs ready to be loaded at the wiki */
 $(function () {
-console.log("Test Log");
+
+
 //This is a list of all checklist. Add to this array when you add a new checklist. For any other edits to the checklists, see the instructions here: https://github.com/worldbank/DIMEwiki/Topics/Checklists
 checklists = [
   'chk_surveyprep', 'chk_questcont', 'chk_questdata',
@@ -10,77 +11,55 @@ checklists = [
 
 //Loop over all the checklists
 for (chkNum in checklists) {
-  //console.log("ChecklistNum " + chkNum);
-  //console.log("ChecklistName " + checklists[chkNum]);
 
+  //Get checklist name
   var chkName = checklists[chkNum];
+
   //Test if a div with the name of the checklist exist on the article just loaded
   if (document.getElementById(chkName) != null ) {
-    console.log("This ChecklistName " + chkName);
+
     //If a div with that name exist, generate the checklist and put it in that div
     getChecklist(chkName);
-
   }
 }
 
-
- // if (document.getElementById("dimewiki-chk-surveyprep") != null ) {
- //   //console.log(chk_surveyprep);
- //   document.getElementById('dimewiki-chk-surveyprep').innerHTML = checklistTable(chk_surveyprep);
- // }
- // if (document.getElementById("dimewiki-chk-questcont") != null ) {
- //   //console.log(questionnaire_chk_content);
- //   document.getElementById('dimewiki-chk-questcont').innerHTML = checklistTable(questionnaire_chk_content);
- // }
- // if (document.getElementById("dimewiki-chk-questdata") != null ) {
- //   //console.log(questionnaire_chk_data);
- //   document.getElementById('dimewiki-chk-questdata').innerHTML = checklistTable(questionnaire_chk_data);
- // }
- // if (document.getElementById("dimewiki-chk-microdata") != null ) {
- //   //console.log(chk_microdata);
- //   document.getElementById('dimewiki-chk-microdata').innerHTML = checklistTable(chk_microdata);
- // }
- // if (document.getElementById("dimewiki-chk-datacleaning") != null ) {
- //   //console.log(chk_datacleaning);
- //   document.getElementById('dimewiki-chk-datacleaning').innerHTML = checklistTable(chk_datacleaning);
- // }
-
-
-
-
-
 function getChecklist(chklistname) {
 
- var requri   =   'https://api.github.com/repos/worldbank/DIMEwiki/contents/Topics/Checklists/checklists/'+ chklistname + '.js?ref=checklistAjax';
+ var requri   =   'https://api.github.com/repos/worldbank/DIMEwiki/contents/Topics/Checklists/checklists/'+ chklistname + '.js';
 
- console.log(chklistname);
+ //Call the GitHub API
+ requestJSON(requri, function(json) {
 
-  requestJSON(requri, function(json) {
-
+   //If call unsuccessful, give error message
    if(json.message == "Not Found" ) {
-      document.getElementById(chklistname).innerHTML = "<h2>Checklist not found, check the name of the checklist</h2>"
+      document.getElementById(chklistname).innerHTML = "<h3>Checklist not found, check the name of the checklist</h3>"
 
- } else {
+    } else {
 
-     //File found start by decoding content
+      //Call is successfull
 
-     //Source https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
-     function b64DecodeUnicode(str) {
-       // Going backwards: from bytestream, to percent-encoding, to original string.
-         return decodeURIComponent(atob(str).split('').map(function(c) {
-           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-       }).join(''));
+
+      //Function to decode result from GitHub
+      function b64DecodeUnicode(str) {
+         //Source https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+         // Going backwards: from bytestream, to percent-encoding, to original string.
+           return decodeURIComponent(atob(str).split('').map(function(c) {
+             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+         }).join(''));
+       }
+
+       //Decode result and put it in parse it to JSON format
+       var checklistSTR = b64DecodeUnicode(json.content);
+       var checklist = JSON.parse("[" + checklistSTR + "]");
+
+       //Create the checklist table and put it to the html div.
+       document.getElementById(chklistname).innerHTML =  createChecklistTable(checklist,chklistname);
+
      }
-
-     var checklistSTR = b64DecodeUnicode(json.content);
-     var checklist = JSON.parse("[" + checklistSTR + "]");
-
-     document.getElementById(chklistname).innerHTML =  checklistTable(checklist);
-
-   }
-}); // end requestJSON Ajax call
+  }); // end requestJSON Ajax call
 }
 
+//The function that calls the GitHub API
 function requestJSON(url, callback) {
  $.ajax({
    url: url,
@@ -91,13 +70,9 @@ function requestJSON(url, callback) {
 }
 
 
-
-
-
-
 /* Functions needed for checklists */
 
-function checklistTable(chkarray) {
+function createChecklistTable(chkarray, chkName) {
 
  var tableString = '<table class="chk_table">';
  tableString += '<tr><th rowspan="5" colspan="3" style="text-align: center;"><img alt="DIME Image" src="/images/a/a8/DIMEi2i.png" class="chk_logo_img"></th>';
@@ -113,8 +88,6 @@ var levelTracker = [0]
    var thisIndex = thisLevel - 1;
    var thisItem =chkarray[row][1];
 
-
-
    if ( levelTracker.length > thisLevel) {
      levelTracker.splice(thisLevel, levelTracker.length - (thisLevel));
    } else if ( levelTracker.length < thisLevel) {
@@ -123,15 +96,16 @@ var levelTracker = [0]
 
    ++levelTracker[thisIndex]
 
-
    var number = levelTracker.join(".")
 
    tableString += checklistRow(thisLevel, thisItem, number)
  }
- tableString += '</table>'
+
+ var tableFooter = '<tr><td colspan="4">The checklist are edited through GitHub. This checklist corresponds to the file with the name <b>' + chkName+ '.js</b>. To read a simple step by step guide on how to edit the checklist, see this documentation:<br><a href="https://github.com/worldbank/DIMEwiki/tree/master/Topics/Checklists">https://github.com/worldbank/DIMEwiki/tree/master/Topics/Checklists</a>.</td></tr>'
+
+ tableString += tableFooter +'</table>'
 
  return tableString
-
 }
 
 
