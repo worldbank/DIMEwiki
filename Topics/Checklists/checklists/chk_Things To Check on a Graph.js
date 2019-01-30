344927b8b3105787fcfaaebe7b2f5f595f631de3
xyc9471 <46945444+xyc9471@users.noreply.github.com>
2019-01-30 16:23:25 -0500

    Create chk_Things To Check on a Graph.js
    This is a list for users to check before they finalizing their graphs and charts.

---
+++ Topics/Checklists/checklists/chk_Things To Check on a Graph.js
@@ -0,0 +1,5 @@
+[1, "Are x-axis and y-axis clearly labeled?"],
+[2, "Are the units of your data clearly defined?"],
+[3, "Do the axis unit match the graph unit?"],
+[4, "Are the colors printed well in black and white?"],
+[5, "Will color blind people be able to understand"]
