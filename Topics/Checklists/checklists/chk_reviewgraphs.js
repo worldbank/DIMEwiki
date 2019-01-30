cd06fa00c25eb42e8dbfbe87b0254dd037b8648a
Luiza <luiza.c.andrade@gmail.com>
2019-01-30 17:35:21 -0500

    Update chk_reviewgraphs.js

--- Topics/Checklists/checklists/chk_reviewgraphs.js
+++ Topics/Checklists/checklists/chk_reviewgraphs.js
@@ -1,5 +1,7 @@
-[1, "Are x-axis and y-axis clearly labeled?"],
-[2, "Are the units of your data clearly defined?"],
-[3, "Do the axis unit match the graph unit?"],
-[4, "Are the colors printed well in black and white?"],
-[5, "Will color blind people be able to understand"]
+[1,"Graph content"],
+   [2,"Are x-axis and y-axis clearly labeled?"],
+   [2,"Are the units of your data clearly defined?"],
+   [2,"Do the axis unit match the graph unit?"],
+[1,"Colors"],
+   [2,"Will colors print well in black and white if needed?"],
+   [2,"How are the colors affected by color blindness?"]
