e5d02228c646d5c956e987e44e37ab3a068b7688
Avnish <62024812+Avnish95@users.noreply.github.com>
2021-01-21 13:11:20 -0500

    Create chk_surveyprotocols.js

---
+++ Topics/Checklists/checklists/chk_surveyprotocols.js
@@ -0,0 +1,29 @@
+ [1,"Interview Scheduling"],
+   [2,"Is there a particular time of day when respondents are more / less likely to be available?"],
+   [2,"What is the most effective way to communicate with respondents and schedule appointments?"],
+ [1,"Access to infrastructure"],
+   [2,"Is there electricity?"],
+    [3,"How frequent are blackouts?"],
+    [3,"Are generators available?"],
+    [3,"Do teams need to carry fuel and/or generators?"],
+   [2,"Will there be any internet access?"],
+    [3,"Are mobile internet connections (dongles) available?"],
+    [3,"Is there mobile data coverage?"],
+    [3,"How is the coverage of mobile phone networks? (Note: Please check availability of any common networks, and advise field teams accordingly.)"],
+ [1,"Sampling"],
+   [2,"Does the sampling protocol work well in the field?"],
+   [2,"Is the replacement strategy effective?"],
+   [2,"What challenges are enumerators likely to face? (Note: These should be clarified during enumerator training.)"],
+   [2,"If your team is working from an existing sampling frame, is it up to date?"],
+   [2,"If your tean is doing a listing, how much time will you allocate to this in the field plan?"],
+   [2,"How will you ensure that sampling units (e.g. households, firms, traders) are NOT skipped?"],
+ [1,"Geo-data (Applies if your team is using geospatial data)"],
+   [2,"Will you use the tablet or separate GPS?"],
+    [3,"If using a GPS unit, did you test the protocol for saving with filenames where unique IDs are linked to respondents?"],
+    [3,"How long does it take to lock in a signal? Is this time within reasonable bounds? (Y/N)"],
+    [3,"Is the data point accurate? (Y/N)"],
+    
+   
+    
+ 
+ 
